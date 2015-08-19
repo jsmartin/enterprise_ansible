e64968d96f29fafff811344ee80409d0bda1ae00
James Martin <jmartin@ansible.com>
2015-08-19 11:38:29 -0400

    fixed paths

--- source_me.sh
+++ source_me.sh
@@ -2,6 +2,6 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export ANSIBLE_ROLES_PATH=$DIR/roles
-export ANSIBLE_CALLBACK_PLUGINS=$DIR/callback
-export ANSIBLE_FILTER_PLUGINS=$DIR/filter
+export ANSIBLE_CALLBACK_PLUGINS=$DIR/plugins/callback
+export ANSIBLE_FILTER_PLUGINS=$DIR/plugins/filter
 export ANSIBLE_LIBRARY=$DIR/library
