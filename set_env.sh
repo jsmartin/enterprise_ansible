20b6aa6a6828522feb9787b8dcbe544606a7a942
James Martin <jmartin@ansible.com>
2015-08-20 10:29:21 -0400

    not needed

--- set_env.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash -ex
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export ANSIBLE_ROLES_PATH=$DIR/roles
-export ANSIBLE_CALLBACK_PLUGINS=$DIR/callback
-export ANSIBLE_FILTER_PLUGINS=$DIR/filter
-export ANSIBLE_LIBRARY=$DIR/library
